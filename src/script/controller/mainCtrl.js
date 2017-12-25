23ba1f4339e1d71cd873d44dfcc6923b8bb67293
csdoker <csd758371536@qq.com>
2017-12-26 00:21:59 +0800

    main_header

--- src/script/controller/mainCtrl.js
+++ src/script/controller/mainCtrl.js
@@ -1,3 +1,8 @@
+/**
+* Author: csdoker
+* CreateTime: 2017/12/25
+* Tips: 博客主页——文章列表页控制器
+*/
 BlogApp.controller('mainCtrl', ['$http', '$scope',
   function($http, $scope) {
     // $http.get('/data/positionList.json').then(function(response) {
--- src/script/directive/header.js
+++ src/script/directive/header.js
@@ -1,3 +1,8 @@
+/**
+* Author: csdoker
+* CreateTime: 2017/12/25
+* Tips: 页面公用——头部指令模块
+*/
 BlogApp.directive('appHeader', [function() {
   return {
     templateUrl: 'view/template/header.html',
