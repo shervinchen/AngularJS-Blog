db6f2776c1d17fe0ec64d3104e6f58b9c43bd1ff
csdoker <csd758371536@qq.com>
2017-12-27 22:22:59 +0800

    main_container-left

---
+++ src/script/directive/postList.js
@@ -0,0 +1,15 @@
+/**
+* Author: csdoker
+* CreateTime: 2017/12/25
+* Tips: main-文章列表指令模块
+*/
+BlogApp.directive('appPostList', [function() {
+  return {
+    templateUrl: 'view/template/postList.html',
+    restrict: 'ECAM',
+    replace: true,
+    link: function ($scope) {
+
+    }
+  };
+}]);
