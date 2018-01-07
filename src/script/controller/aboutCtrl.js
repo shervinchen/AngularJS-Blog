71f84e062077c5aa8b66de28a0e7eb708b0ab98d
csdoker <csd758371536@qq.com>
2018-01-07 17:31:50 +0800

    modify

--- src/script/controller/aboutCtrl.js
+++ src/script/controller/aboutCtrl.js
@@ -1,3 +1,8 @@
+/**
+* Author: csdoker
+* CreateTime: 2017/1/6
+* Tips: 博客关于页控制器
+*/
 BlogApp.controller('aboutCtrl', ['$http', '$scope',
   function($http, $scope) {
     // $http.get('/data/positionList.json').then(function(response) {
