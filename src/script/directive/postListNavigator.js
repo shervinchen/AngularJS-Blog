2d8e03296e9c0311223d3f8fe7d0ebfc4daee8a3
csdoker <csd758371536@qq.com>
2018-01-15 11:17:02 +0800

    modify

--- src/script/directive/postListNavigator.js
+++ src/script/directive/postListNavigator.js
@@ -4,19 +4,23 @@
  * Tips: main-列表分页指令模块
  */
 BlogApp.directive('appPostListNavigator', ['PAGE_FIRST', '$stateParams', function(PAGE_FIRST, $stateParams) {
-  return {
-    templateUrl: 'view/template/postListNavigator.html',
-    restrict: 'ECAM',
-    replace: true,
-    controller: function($scope) {
-      $scope.isShowPre = false;
-      if ($stateParams.page > PAGE_FIRST) {
-        $scope.isShowPre = true;
-      }
-      $scope.isShowNext = true;
+	return {
+		templateUrl: 'view/template/postListNavigator.html',
+		restrict: 'ECAM',
+		replace: true,
+		controller: function($scope) {
+			// 判断是否显示分页上一步按钮
+			$scope.isShowPre = false;
+			if ($stateParams.page > PAGE_FIRST) {
+				$scope.isShowPre = true;
+			} else {
+				$scope.isShowPre = false;
+			}
+			// 判断是否显示分页下一步按钮
+			$scope.isShowNext = true;
 		},
-    link: function($scope) {
-
-    }
-  };
+		link: function($scope) {
+			
+		}
+	};
 }]);
