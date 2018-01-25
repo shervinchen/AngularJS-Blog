d5430dcbac573d39ac9a92b114a38b24dfa06954
csdoker <csd758371536@qq.com>
2018-01-25 18:01:50 +0800

    modify

---
+++ src/script/directive/postCategories.js
@@ -0,0 +1,18 @@
+/**
+* Author: csdoker
+* CreateTime: 2018/1/25
+* Tips: 文章分类模块指令
+*/
+BlogApp.directive('appPostCategories', [function() {
+  return {
+    templateUrl: 'view/template/postCategories.html',
+    restrict: 'ECAM',
+    replace: true,
+    scope: {
+      postCategoriesDatas: '='
+    },
+    link: function ($scope) {
+    	
+    }
+  };
+}]);
