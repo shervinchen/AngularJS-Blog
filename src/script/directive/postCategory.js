d9d54e0fd8398b5620a209a1ba187e7b68d5e75a
csdoker <csd758371536@qq.com>
2018-02-01 18:08:51 +0800

    modify

--- src/script/directive/postCategory.js
+++ src/script/directive/postCategory.js
@@ -3,13 +3,21 @@
 * CreateTime: 2017/12/31
 * Tips: 文章分类指令模块
 */
-BlogApp.directive('appPostCategory', [function() {
+BlogApp.directive('appPostCategory', ['$http', function($http) {
   return {
     templateUrl: 'view/template/postCategory.html',
     restrict: 'ECAM',
     replace: true,
     link: function ($scope) {
-
+		$http({
+			method: 'GET',
+			url: 'http://localhost:3000/postcategory'
+		}).then(function(response) {
+			$scope.categories = response.data;
+		}, function(response) {
+			// 请求失败执行代码
+			console.log('request failed');
+		});
     }
   };
 }]);
